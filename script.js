fb5c63216a42d10469ebba24f7bb14f624b89f5d
Glitch (hello-webpage) <none>
2020-10-14 03:35:11 +0000

    🐓🌶 Checkpoint ./script.js:58163/237 ./style.css:58163/439 ./index.html:58163/2427

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ d3.csv('coffee-house-chains.csv', d3.autoType).then(data=>{
   
   console.log('data,', data);
   
-  d3.select()
+  const margin = { top:20, left:20, right:20, bottom:20 }
+  const width = 600 - margin.left - margin.right;
+  const height = 450 - margin.top - margin.bottom;
+  
+  d3.select('.chart').append
 })
